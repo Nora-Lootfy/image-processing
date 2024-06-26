3a406040db48cfec9307101ae747703a03c19895
Nourhan Abdelaziz <nourhan33abdelaziz@gmail.com>
2024-06-26 12:28:48 +0300

    sharpen image, noise removal, FFT & DCT, image enhancement

--- lectures/brightness and contrast/test.m
+++ lectures/brightness and contrast/test.m
@@ -2,8 +2,10 @@ clear
 close all
 clc
 
+% read image
 image = imread('image.jpg');
 
+% apply function to it
 adjustImage(image, 2, 2, '2: contrast, 2: brightness')
 
 adjustImage(image, -2, 0.5, '0.5: contrast, -2: brightness')
